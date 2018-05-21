3859a46dbbb48ca1801fd4f90cb3e2efbbc85ed0
isuru <prabath230@gmail.com>
2018-05-21 16:36:05 +0530

    fist step driver and customor

---
+++ app/src/main/java/com/example/isuru/sltaxi/CustomorLoginActivity.java
@@ -0,0 +1,13 @@
+package com.example.isuru.sltaxi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CustomorLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customor_login);
+    }
+}
