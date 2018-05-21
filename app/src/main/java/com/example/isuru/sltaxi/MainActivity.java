69ad1f8b65b4590197089dac066e9b9aa44cb625
isuru <prabath230@gmail.com>
2018-05-21 17:05:33 +0530

    fist step driver and customor

---
+++ app/src/main/java/com/example/isuru/sltaxi/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.isuru.sltaxi;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Thread thread = new Thread() {
+
+            @Override
+
+            public void run() {
+
+                try {
+
+                    sleep(3000);
+
+                } catch (Exception e) {
+
+                    e.printStackTrace();
+
+                } finally {
+                    Intent welcomeintent = new Intent(MainActivity.this, WelcomeActivity.class);
+                    startActivity(welcomeintent);
+                }
+            }
+
+
+        };
+        thread.start();
+    }
+
+        @Override
+
+                protected void onPause() {
+            super.onPause();
+            finish();
+        }
+    }
+
+
