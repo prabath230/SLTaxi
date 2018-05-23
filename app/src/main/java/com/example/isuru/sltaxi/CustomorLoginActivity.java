23ef1c39611922979cadc3846beaf86ec25ef50d
isuru <prabath230@gmail.com>
2018-05-23 16:28:46 +0530

    fist step driver and customor Part 4 login

--- app/src/main/java/com/example/isuru/sltaxi/CustomorLoginActivity.java
+++ app/src/main/java/com/example/isuru/sltaxi/CustomorLoginActivity.java
@@ -2,12 +2,43 @@ package com.example.isuru.sltaxi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class CustomorLoginActivity extends AppCompatActivity {
+
+    private Button customorlogbtn;
+    private Button customorregbtn;
+    private TextView customorstatus;
+    private TextView customorloglink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customor_login);
+
+        customorlogbtn = findViewById(R.id.customorlog_btn);
+        customorregbtn = findViewById(R.id.customorreg_btn);
+        customorstatus = findViewById(R.id.customor_status);
+        customorloglink = findViewById(R.id.customorlog_link);
+
+        customorregbtn.setVisibility(View.INVISIBLE);
+        customorregbtn.setEnabled(false);
+
+
+
+        customorloglink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                customorlogbtn.setVisibility(View.INVISIBLE);
+                customorregbtn.setVisibility(View.INVISIBLE);
+                customorstatus.setText("Register Customer");
+                customorregbtn.setVisibility(View.VISIBLE);
+                customorregbtn.setEnabled(true);
+
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/isuru/sltaxi/DriverLoginActivity.java
+++ app/src/main/java/com/example/isuru/sltaxi/DriverLoginActivity.java
@@ -2,12 +2,43 @@ package com.example.isuru.sltaxi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class DriverLoginActivity extends AppCompatActivity {
+
+    private Button Driverlogbtn;
+    private Button Driverregbtn;
+    private TextView Driverstatus;
+    private TextView Driverregisterlink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver_login);
+
+
+        Driverlogbtn = findViewById(R.id.Driverlog_btn);
+        Driverregbtn = findViewById(R.id.Driverregi_btn);
+        Driverstatus = findViewById(R.id.Driver_status);
+        Driverregisterlink = findViewById(R.id.Driverregister_link);
+
+        Driverregbtn.setVisibility(View.INVISIBLE);
+        Driverregbtn.setEnabled(false);
+
+        Driverregisterlink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Driverlogbtn.setVisibility(View.INVISIBLE);
+                Driverregbtn.setVisibility(View.INVISIBLE);
+                Driverstatus.setText("Register Customer");
+                Driverregbtn.setVisibility(View.VISIBLE);
+                Driverregbtn.setEnabled(true);
+
+
+            }
+        });
+
     }
 }
